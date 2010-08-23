97c9c3764df19e4dba5882105049d658dac85cec
Matthew Horan <matt@matthoran.com>
2010-08-23 19:05:21 -0400

    Initial commit!

---
+++ src/org/bostonandroid/umbrellatoday/UmbrellaToday.java
@@ -0,0 +1,15 @@
+package org.bostonandroid.umbrellatoday;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class UmbrellaToday extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
