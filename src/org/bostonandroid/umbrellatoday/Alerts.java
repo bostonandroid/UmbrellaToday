13087a7e5be352ebbaa0c4384707ac46dc3fd58c
Mike Burns <mburns@thoughtbot.com>
2010-09-14 21:34:37 -0400

    Prettier 'Add Alert' button

--- src/org/bostonandroid/umbrellatoday/Alerts.java
+++ src/org/bostonandroid/umbrellatoday/Alerts.java
@@ -5,28 +5,28 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
-import android.widget.ListView;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 public class Alerts extends ListActivity {
   @Override
   public void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     
-    String[] alerts = {"Add Alert"};
+    String[] alerts = {};
     
     setContentView(R.layout.alerts);
     setListAdapter(new ArrayAdapter(this, 
          android.R.layout.simple_list_item_1,
          alerts));
-  }
-  
-  @Override
-  protected void onListItemClick(ListView parent, View view, int position, long id) {
-    super.onListItemClick(parent, view, position, id);
-    if (position == 0) {
-      Intent i = new Intent(Intent.ACTION_VIEW);
-      i.setClass(Alerts.this, NewAlert.class);
-      startActivity(i);
-    }
+
+    LinearLayout addAlert = (LinearLayout)findViewById(R.id.add_alert);
+    addAlert.setOnClickListener(new View.OnClickListener() {
+      public void onClick(View v) {
+        Intent i = new Intent(Intent.ACTION_VIEW);
+        i.setClass(Alerts.this, NewAlert.class);
+        startActivity(i);
+      }
+    });
   }
 }
