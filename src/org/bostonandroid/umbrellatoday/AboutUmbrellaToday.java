ad85743af7290b14f8c16854b8971c7667470e1b
Matthew Horan <matt@matthoran.com>
2010-09-29 19:47:19 -0400

    There's a getString method!

--- src/org/bostonandroid/umbrellatoday/AboutUmbrellaToday.java
+++ src/org/bostonandroid/umbrellatoday/AboutUmbrellaToday.java
@@ -12,7 +12,7 @@ public class AboutUmbrellaToday extends Activity
     super.onCreate(savedInstanceState);
     setContentView(R.layout.about);
     TextView tv = (TextView) findViewById(R.id.about);
-    tv.setText(Html.fromHtml(getText(R.string.about_text).toString()));
+    tv.setText(Html.fromHtml(getString(R.string.about_text)));
     tv.setMovementMethod(LinkMovementMethod.getInstance());
   }
 }
